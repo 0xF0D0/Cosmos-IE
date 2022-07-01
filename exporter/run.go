d47302dc8afae574bd31606cf544b5e973a9e904
fodo <goodbumsu@gmail.com>
2022-07-01 23:27:47 +0900

    add osmosis
    Signed-off-by: fodo <goodbumsu@gmail.com>

--- exporter/run.go
+++ exporter/run.go
@@ -103,11 +103,28 @@ func setConfig(chain string) {
 		Bech32PrefixValPub := Bech32MainPrefix + PrefixValidator + PrefixOperator + PrefixPublic
 		Bech32PrefixConsAddr := Bech32MainPrefix + PrefixValidator + PrefixConsensus
 		Bech32PrefixConsPub := Bech32MainPrefix + PrefixValidator + PrefixConsensus + PrefixPublic
-			config := sdk.GetConfig()
-			config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
-			config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
-			config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
-			config.Seal()
+		config := sdk.GetConfig()
+		config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+		config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+		config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+		config.Seal()
+	case "osmosis":
+		Bech32MainPrefix := "osmo"
+		PrefixValidator := "val"
+		PrefixConsensus := "cons"
+		PrefixPublic := "pub"
+		PrefixOperator := "oper"
+		Bech32PrefixAccAddr := Bech32MainPrefix
+		Bech32PrefixAccPub := Bech32MainPrefix + PrefixPublic
+		Bech32PrefixValAddr := Bech32MainPrefix + PrefixValidator + PrefixOperator
+		Bech32PrefixValPub := Bech32MainPrefix + PrefixValidator + PrefixOperator + PrefixPublic
+		Bech32PrefixConsAddr := Bech32MainPrefix + PrefixValidator + PrefixConsensus
+		Bech32PrefixConsPub := Bech32MainPrefix + PrefixValidator + PrefixConsensus + PrefixPublic
+		config := sdk.GetConfig()
+		config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
+		config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
+		config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
+		config.Seal()
 	}
 
 	config.Seal()
